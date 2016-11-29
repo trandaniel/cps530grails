bb56dbda3221dd4ee1c64a5376bedfceacac88b7
Samuel Dindyal <sam.dindyal@me.com>
2016-11-29 12:25:25 -0500

    Bug fix for active navbar element

--- grails-app/assets/javascripts/custom.js
+++ grails-app/assets/javascripts/custom.js
@@ -1,8 +1,8 @@
 $('a.navbar-item').on('click', function(e) {
   e.preventDefault();
   var $this = $(this);
-  $this.toggleClass('active');
 
-  $('a.navbar-item.active').toggleClass('active');
+  $('a.navbar-item.active').removeClass('active');
+  $this.toggleClass('active');
   $('div#content').fadeOut('fast').load($this.attr('href') + " div#content").fadeIn('fast');
 });
